4c905df18e09b329ee38f01418587a7b1f4c88ac
yaldaalemi <yaldalami@gmail.com>
2023-02-06 21:49:10 -0800

    added

---
+++ static/js/app.js
@@ -0,0 +1,4 @@
+const samples = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+d3.json("samples").then( data => {
+    console.log(data);
+})
